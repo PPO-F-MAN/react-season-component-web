99e31e9ba5770a1335c36d958cc05b13f7f3e1a5
dante01yoon <bunggl@naver.com>
2022-02-20 22:59:19 +0900

    feat: Controller Component markup
    Co-authored-by: deveeni <deveeni@users.noreply.github.com>

--- src/components/Controller/styled.tsx
+++ src/components/Controller/styled.tsx
@@ -1,11 +1,50 @@
 import styled from '@emotion/styled';
 
+export type ControllerColorType = 'weather' | 'time' | 'season';
+
 export const Container = styled.div`
   position: fixed;
-  right: 0;
   top: 0;
-  width: 400px;
-  background-color: black;
-  height: 550px;
+  right: 0;
+  width: 580px;
+  height: 780px;
+`;
+
+export const Foreground = styled.div<{ type: ControllerColorType }>`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  width: 550px;
+  background-color: ${({ type }) => {
+    switch (type) {
+      case 'weather':
+        return '#FFF9FD';
+      case 'time':
+        return '#FFEEDA';
+      default:
+        return '#FFF9F3';
+    }
+  }};
+  height: 750px;
   border-radius: 50px 50px 0px 300px;
+  z-index: 1;
+`;
+
+export const Background = styled.div<{ type: ControllerColorType }>`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 500px;
+  height: 575px;
+  background-color: ${({ type }) => {
+    switch (type) {
+      case 'weather':
+        return '#FFE7F5';
+      case 'time':
+        return '#FFC785';
+      default:
+        return '#FFF3E7';
+    }
+  }};
+  z-index: 0;
 `;
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Controller } from '../components/Controller';
+export { default as Sidebar } from '../components/Sidebar';
