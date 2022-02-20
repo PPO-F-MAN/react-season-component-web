d4b543d0516771bf6a48a633708b980e25e4b9ba
yongwookLee <yongw00k@g.skku.edu>
2022-02-20 22:36:47 +0900

    chore: Set store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+
+import { Provider } from 'react-redux';
 import App from './App';
+
+import store from './redux/configStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/redux/configStore.hooks.ts
+++ src/redux/configStore.hooks.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './configStore';
 
-export const setTheme = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-export const getTheme: TypedUseSelectorHook<RootState> = useSelector;
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const getTheme = () => useAppSelector(state => state.theme.currentTheme);
