72f68cbc71d99895b803d46aac30ea70be01d33c
정현수 <jung660317@naver.com>
2022-02-20 20:58:15 +0900

    feat: 간단히 구조화

--- src/pages/Time/index.tsx
+++ src/pages/Time/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import * as Styled from './styled';
+import Controller from '../../components/Controller';
 
 function Time() {
-	return (
-		<div>Time</div>
-	);
+  return (
+    <>
+      <Controller>dd</Controller>
+      <Styled.Container>
+        <Styled.Title>Time</Styled.Title>
+        <Styled.SubTitle>Preview</Styled.SubTitle>
+        <Styled.Card />
+        <Styled.SubTitle>Example Code</Styled.SubTitle>
+        <Styled.CodeContainer />
+      </Styled.Container>
+    </>
+  );
 }
-
 
 export default Time;
