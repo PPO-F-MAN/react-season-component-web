1a10e3abf72f3c968aa1b05eb6e4d2d03f32854a
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-02-21 03:12:04 +0900

    feat: Update Season page

---
+++ src/pages/Season/styled.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+interface CardProps {
+  width: number;
+  height: number;
+}
+
+export const Container = styled.div`
+  position: relative;
+  padding-left: 140px;
+  padding-bottom: 50px;
+  width: 80%;
+`;
+
+export const Title = styled.p`
+  font-size: 64px;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 36px;
+`;
+
+export const Card = styled.div<CardProps>`
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+`;
+
+export const CodeContainer = styled.code``;
