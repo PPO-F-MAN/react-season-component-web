001403d1680f36ce40fb8fc479abb60661d80384
정현수 <jung660317@naver.com>
2022-02-21 14:32:56 +0900

    bug: 이미지 경로 수정

--- src/components/Sidebar/styled.tsx
+++ src/components/Sidebar/styled.tsx
@@ -64,7 +64,7 @@ export const Container = styled.div<{ type: SidebarColorType }>`
 // SlidingPointer,
 
 export const Logo = styled.div`
-  background-image: url('react-season-component-logo.png');
+  background-image: url('https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/react-season-component-logo.png?raw=true');
   background-size: cover;
   width: 80px;
   height: 80px;
@@ -79,7 +79,11 @@ export const ButtonGroup = styled.div`
 
 export const HomeButton = styled.div<{ isDark: boolean }>`
   position: relative;
-  background-image: url(${({ isDark }) => (isDark ? 'home_dark.png' : 'home_light.png')});
+  /* background-image: url(${({ isDark }) => (isDark ? 'home_dark.png' : 'home_light.png')}); */
+  background-image: ${({ isDark }) =>
+    isDark
+      ? 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/home_dark.png?raw=true")'
+      : 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/home_light.png?raw=true")'};
   background-size: cover;
   width: 30px;
   height: 30px;
@@ -89,7 +93,11 @@ export const HomeButton = styled.div<{ isDark: boolean }>`
 
 export const TimeButton = styled.div<{ isDark: boolean }>`
   position: relative;
-  background-image: url(${({ isDark }) => (isDark ? 'time_dark.png' : 'time_light.png')});
+  /* background-image: url(${({ isDark }) => (isDark ? 'time_dark.png' : 'time_light.png')}); */
+  background-image: ${({ isDark }) =>
+    isDark
+      ? 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/time_dark.png?raw=true")'
+      : 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/time_light.png?raw=true")'};
   background-size: cover;
   width: 30px;
   height: 30px;
@@ -99,7 +107,11 @@ export const TimeButton = styled.div<{ isDark: boolean }>`
 
 export const SeasonButton = styled.div<{ isDark: boolean }>`
   position: relative;
-  background-image: url(${({ isDark }) => (isDark ? 'season_dark.png' : 'season_light.png')});
+  /* background-image: url(${({ isDark }) => (isDark ? 'season_dark.png' : 'season_light.png')}); */
+  background-image: ${({ isDark }) =>
+    isDark
+      ? 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/season_dark.png?raw=true")'
+      : 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/season_light.png?raw=true")'};
   background-size: cover;
   width: 30px;
   height: 30px;
@@ -109,7 +121,12 @@ export const SeasonButton = styled.div<{ isDark: boolean }>`
 
 export const WeatherButton = styled.div<{ isDark: boolean }>`
   position: relative;
-  background-image: url(${({ isDark }) => (isDark ? 'weather_dark.png' : 'weather_light.png')});
+  /* background-image: url(${({ isDark }) =>
+    isDark ? 'weather_dark.png' : 'weather_light.png'}); */
+  background-image: ${({ isDark }) =>
+    isDark
+      ? 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/weather_dark.png?raw=true")'
+      : 'url("https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/weather_light.png?raw=true")'};
   background-size: cover;
   width: 30px;
   height: 30px;
@@ -125,7 +142,8 @@ export const ForMargin = styled.div`
 
 export const SlidingPointer = styled.div<{ page: 'Home' | 'Time' | 'Season' | 'Weather' }>`
   position: relative;
-  background-image: url('sliding_pointer.png');
+  /* background-image: url('sliding_pointer.png'); */
+  background-image: url('https://github.com/PPO-F-MAN/react-season-component-web/blob/develop/public/sliding_pointer.png?raw=true');
   background-size: cover;
   width: 80px;
   height: 100px;
