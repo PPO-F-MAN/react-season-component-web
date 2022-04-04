64071c76456e991931328b96c447e7dfdfae9220
useonglee <useong0830@naver.com>
2022-04-04 21:05:08 +0900

    #33 test: Change typeChecker component storybook error

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-knobs',
     '@storybook/preset-create-react-app',
   ],
   framework: '@storybook/react',
